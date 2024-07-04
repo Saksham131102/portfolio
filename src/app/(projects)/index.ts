a0b27182aae291a5b0a6f9f40746d951a91e8e48
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-07-04 22:29:50 +0530

    Favicons Updated

--- src/app/(projects)/index.ts
+++ src/app/(projects)/index.ts
@@ -3,6 +3,10 @@ const projects = [
     image: "/images/snapgram.png",
     name: "snapgram",
   },
+  {
+    image: "/images/streamchat.png",
+    name: "streamchat",
+  },
 ];
 
 export default projects;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./(components)/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icon-48x48.png" sizes="48x48" />
+        <link rel="icon" href="/icon-72x72.png" sizes="72x72" />
+        <link rel="icon" href="/icon-96x96.png" sizes="96x96" />
+        <link rel="icon" href="/icon-144x144.png" sizes="144x144" />
+        <link rel="icon" href="/icon-192x192.png" sizes="192x192" />
+        <link rel="icon" href="/icon-512x512.png" sizes="512x512" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="apple-mobile-web-app-title" content="AppName" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
       <body className={inter.className}>
         <div className="bg"></div>
         <Navbar />
