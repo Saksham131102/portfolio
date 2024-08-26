4b14ae0780df53a90e7242850b1f8f866e703b20
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-08-26 13:24:53 +0530

    projects order updated

--- src/app/(projects)/index.ts
+++ src/app/(projects)/index.ts
@@ -1,15 +1,15 @@
 const projects = [
   {
-    image: "/images/snapgram.png",
-    name: "snapgram",
-    github: "https://github.com/Saksham131102/snapgram",
-    hostedLink: "https://snapgram-rho-seven.vercel.app",
-  },
-  {
     image: "/images/streamchat.png",
     name: "streamchat",
     github: "https://github.com/Saksham131102/snapgram",
     hostedLink: "https://streamchat1.netlify.app/",
+  },
+  {
+    image: "/images/snapgram.png",
+    name: "snapgram",
+    github: "https://github.com/Saksham131102/snapgram",
+    hostedLink: "https://snapgram-rho-seven.vercel.app",
   },
 ];
 
