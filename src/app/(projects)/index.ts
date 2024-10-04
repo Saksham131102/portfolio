3b6e5d981bd25cabb5b6b15337f8e320c184ac5d
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-10-04 10:11:30 +0530

    new project added

--- src/app/(projects)/index.ts
+++ src/app/(projects)/index.ts
@@ -1,5 +1,12 @@
 const projects = [
   {
+    image:
+      "https://github.com/Saksham131102/CGC-Placements/blob/main/screenshots/home_dashboard.png",
+    name: "CGC-Placement App",
+    github: "https://github.com/Saksham131102/CGC-Placements",
+    hostedLink: "https://cgcstudents.vercel.app",
+  },
+  {
     image: "/images/streamchat.png",
     name: "streamchat",
     github: "https://github.com/Saksham131102/STREAMCHAT",
