dbcde7f15d8e4e78e5024202486d6796f4977df5
Saksham <113179290+Saksham131102@users.noreply.github.com>
2024-12-14 23:46:26 +0530

    docker updated

--- src/app/(components)/Docker.tsx
+++ src/app/(components)/Docker.tsx
@@ -4,34 +4,42 @@ import { IoLogoGithub, IoLogoInstagram, IoLogoLinkedin } from "react-icons/io5";
 const Docker = () => {
   return (
     <div className="absolute left-1/2 transform -translate-x-1/2 w-auto z-50 bottom-4 flex justify-center items-center text-xl">
-      <div className="bg-white text-[#8e8e92] h-14 w-60 rounded-full border border-gray-300 shadow-md flex justify-center items-center gap-4">
-        <a href="#home">
-          <FiHome className="text-lg hover:text-black hover:text-2xl transition-all duration-300" />
+      <div className="bg-white text-[#8e8e92] h-14 px-5 rounded-full border border-gray-300 shadow-md flex justify-center items-center gap-4">
+        <a
+          href="#home"
+          className="active:bg-gray-300 active:text-black rounded-full h-9 w-9 flex justify-center items-center"
+        >
+          <FiHome className="text-xl" />
         </a>
         <div className="border border-r h-8"></div>
         <a
           href="https://www.linkedin.com/in/saksham013/"
           target="_blank"
           rel="noopener noreferrer"
+          className="active:bg-gray-300 active:text-black rounded-full h-9 w-9 flex justify-center items-center"
         >
-          <IoLogoLinkedin className="text-lg hover:text-black hover:text-2xl transition-all duration-300" />
+          <IoLogoLinkedin className="text-xl" />
         </a>
         <a
           href="https://github.com/Saksham131102"
           target="_blank"
           rel="noopener noreferrer"
+          className="active:bg-gray-300 active:text-black rounded-full h-9 w-9 flex justify-center items-center"
         >
-          <IoLogoGithub className="text-lg hover:text-black hover:text-2xl transition-all duration-300" />
+          <IoLogoGithub className="text-xl" />
         </a>
         <a
           href="https://www.instagram.com/flemoid.___/"
           target="_blank"
           rel="noopener noreferrer"
+          className="active:bg-gray-300 active:text-black rounded-full h-9 w-9 flex justify-center items-center"
         >
-          <IoLogoInstagram className="text-lg hover:text-black hover:text-2xl transition-all duration-300" />
+          <IoLogoInstagram className="text-xl" />
         </a>
         <div className="border border-r h-8"></div>
-        <FiSun className="text-lg hover:text-black hover:text-2xl transition-all duration-300" />
+        <div className="active:bg-gray-300 active:text-black rounded-full h-9 w-9 flex justify-center items-center">
+          <FiSun className="text-xl" />
+        </div>
       </div>
     </div>
   );
