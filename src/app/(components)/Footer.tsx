b7b38957c4c2946b1fdfa1c0daf9dbbc7bcb3ceb
Saksham <saksham7104@gmail.com>
2025-09-21 16:41:02 +0530

    footer updated

--- src/app/(components)/Footer.tsx
+++ src/app/(components)/Footer.tsx
@@ -3,7 +3,7 @@ import { BsGlobeCentralSouthAsia } from "react-icons/bs";
 const Footer = () => {
   return (
     <div className="text-sm text-[#8e8e92] dark:text-[#b3b3b3] flex justify-between pb-16">
-      <div>@ 2024 Saksham</div>
+      <div>© {new Date().getFullYear()} Saksham</div>
       <div className="flex items-center gap-2">
         <BsGlobeCentralSouthAsia /> India
       </div>
